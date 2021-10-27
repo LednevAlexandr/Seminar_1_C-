9593bf5048ef51cfc0dee99164473b70bc11b727
alex <you@example.com>
2021-10-27 22:25:11 +0300

    added prog02

---
+++ prog02/Program.cs
@@ -0,0 +1,16 @@
+﻿int x,y,max;
+Console.Write("x:");
+x=Convert.ToInt32(Console.ReadLine());
+Console.Write("y:");
+y=Convert.ToInt32(Console.ReadLine());
+
+if (x<y)
+{
+    max=y;
+}
+else
+{
+    max=x;
+}
+
+Console.WriteLine("Max="+max);
